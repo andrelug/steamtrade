c6e131a10e3be290a41ea4e0b6d3f97f5235d8c0
André Lucas <pamasway@gmail.com>
2017-05-12 04:28:28 -0300

    item blocks initial

--- public/js/index.js
+++ public/js/index.js
@@ -1,18 +1,32 @@
 import noUiSlider from './noUiSlider.min';
-const slider = document.getElementById('slider');
-const minPrice = document.getElementById('minPrice');
-const maxPrice = document.getElementById('maxPrice');
+import wNumb from './wNumb';
 
-noUiSlider.create(slider, {
-	start: [0, 3500],
-	connect: true,
-	range: {
-		'min': 0,
-		'max': 3500
-	}
-});
+(()=>{
+	const slider = document.getElementById('slider');
+	const minPrice = document.getElementById('minPrice');
+	const maxPrice = document.getElementById('maxPrice');
 
-slider.noUiSlider.on('update', function( values, handle ) {
-	minPrice.value = values[handle];
-	maxPrice.value = values[handle];
-});
+	noUiSlider.create(slider, {
+		start: [0, 3500],
+		connect: true,
+		step: 1,
+		format: wNumb({
+			decimals: 0
+		}),
+		range: {
+			'min': 0,
+			'max': 3500
+		}
+	});
+
+	slider.noUiSlider.on('update', function( values, handle ) {
+		minPrice.value = values[0];
+		maxPrice.value = values[1];
+	});
+
+	fetch('http://localhost:3000/api/items', {
+		credentials : 'same-origin'
+	}).then( response => {
+
+	});
+})();
